b19894ecc9792416ce477b3bd674d843582214eb
Mathieu TUDISCO <mathieu@tudisco.fr>
2021-05-18 21:06:28 +0200

    🔧 Add Eslint.

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://vercel.com/new"
           >
             <h2>Deploy &rarr;</h2>
             <p>
