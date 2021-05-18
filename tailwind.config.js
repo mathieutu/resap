11d85595952932912c600a419e41c2686c51ea40
Mathieu TUDISCO <mathieu@tudisco.fr>
2021-05-19 01:31:59 +0200

    🐛 Fix tailwind purge.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
