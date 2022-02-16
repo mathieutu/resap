3f7025b505ff09e980e7a9f9340564dc10858f02
leam <marchaleo1410@gmail.com>
2022-02-16 11:51:35 +0100

    💄 Update style Annuaire

--- src/components/Annuaire/AnnuaireSection.tsx
+++ src/components/Annuaire/AnnuaireSection.tsx
@@ -45,12 +45,14 @@ export const AnnuaireSection = () => (
         </defs>
         <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
       </svg>
+      <div className="h-52"></div>
       <div className="text-center">
-        <h1 className="text-3xl font-extrabold tracking-tight text-blue-default sm:text-4xl lg:pt-4">Annuaire</h1>
+        <h1 className="text-3xl font-extrabold tracking-tight text-blue-default sm:text-4xl">Annuaire</h1>
         <p className="mt-4 text-lg leading-6 text-gray-500">
           Nous travaillons actuellement sur cette fonctionnalité
         </p>
       </div>
+      <div className="h-56"></div>
     </div>
   </div>
 )
