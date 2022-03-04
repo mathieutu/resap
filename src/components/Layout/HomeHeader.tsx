ced7ff8c117541fd8b8a6b1206e75987f6ee07d4
Florentin-bron <73688648+Florentin-bron@users.noreply.github.com>
2022-03-04 16:11:42 +0100

    ✨ Ajout des logos des partenaires dans le header de la home (#15)
    Co-authored-by: Mathieu TUDISCO <mathieu@tudisco.fr>

--- src/components/Layout/HomeHeader.tsx
+++ src/components/Layout/HomeHeader.tsx
@@ -39,43 +39,50 @@ const SearchForm = () => {
 export const HomeHeader = () => (
   <div className="relative bg-white overflow-hidden">
     <div className="max-w-7xl mx-auto">
-      <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-        <Popover>
-          <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-            <nav
+      <Popover>
+        <div className="bg-white flex justify-between lg:px-8 pt-6 px-4 relative sm:px-6 z-30">
+          <div>
+          <nav
               className="relative flex items-center justify-between sm:h-10 lg:justify-start"
               aria-label="Global"
-            >
-              <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                <div className="flex items-center justify-between w-full md:w-auto">
-                  <Link href="/" title="Page d'accueil">
-                    <LogoFull className="text-blue-default w-14" />
-                  </Link>
-                  <div className="-mr-2 flex items-center md:hidden">
-                    <Popover.Button
+          >
+            <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+              <div className="flex items-center justify-between w-full md:w-auto">
+                <Link href="/" title="Page d'accueil">
+                  <LogoFull className="text-blue-default w-14" />
+                </Link>
+                <div className="-mr-2 flex items-center md:hidden">
+                  <Popover.Button
                       className="bg-white rounded-md p-2 inline-flex items-center justify-center text-blue-default hover:text-grey-default hover:bg-grey-light focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-default"
-                    >
-                      <span className="sr-only">Menu principal</span>
-                      <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
+                  >
+                    <span className="sr-only">Menu principal</span>
+                    <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
                 </div>
               </div>
-              <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                {navigationEntries.map((item) => (
+            </div>
+            <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+              {navigationEntries.map((item) => (
                   <Link
-                    key={item.name}
-                    href={item.href}
-                    className="font-medium text-grey-default hover:text-blue-default"
+                      key={item.name}
+                      href={item.href}
+                      className="font-medium text-grey-default hover:text-blue-default"
                   >
                     {item.name}
                   </Link>
-                ))}
-              </div>
-            </nav>
+              ))}
+            </div>
+          </nav>
+        </div>
+          <div className="flex justify-end flex-wrap gap-4 hidden lg:block">
+            <p className="flex gap-8 items-center flex-wrap">
+              <img className="h-10" src="/partenaires/logo-ars.png" alt="Logo ARS" />
+              <img className="h-12" src="/partenaires/logo-gouv.png" alt="Logo gouvernement" />
+            </p>
           </div>
+        </div>
 
-          <Transition
+        <Transition
             as={Fragment}
             enter="duration-150 ease-out"
             enterFrom="opacity-0 scale-95"
@@ -83,42 +90,44 @@ export const HomeHeader = () => (
             leave="duration-100 ease-in"
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
+        >
+          <Popover.Panel
               focus
               className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-10"
-            >
-              <div
+          >
+            <div
                 className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
-              >
-                <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <LogoFull className="text-blue-default w-14" />
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button
-                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-blue-default hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-default"
-                    >
-                      <span className="sr-only">Close main menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
+            >
+              <div className="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <LogoFull className="text-blue-default w-14" />
                 </div>
-                <div className="px-2 pt-2 pb-3 space-y-1">
-                  {navigationEntries.map((item) => (
+                <div className="-mr-2">
+                  <Popover.Button
+                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-blue-default hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-default"
+                  >
+                    <span className="sr-only">Close main menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {navigationEntries.map((item) => (
                     <Link
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 rounded-md text-base font-medium text-grey-default hover:text-blue-default hover:bg-grey-light"
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-grey-default hover:text-blue-default hover:bg-grey-light"
                     >
                       {item.name}
                     </Link>
-                  ))}
-                </div>
+                ))}
               </div>
-            </Popover.Panel>
-          </Transition>
-        </Popover>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+      <div className="relative lg:z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+
 
         <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div className="sm:text-center lg:text-left">
@@ -134,6 +143,14 @@ export const HomeHeader = () => (
             </p>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <SearchForm />
+            </div>
+          </div>
+          <div className="relative lg:right-0 px-1 pt-10 lg:hidden md:block">
+            <div className="flex justify-center flex-wrap gap-4">
+              <p className="flex gap-8 items-center flex-wrap">
+                <img className="h-10" src="/partenaires/logo-ars.png" alt="Logo ARS" />
+                <img className="h-12" src="/partenaires/logo-gouv.png" alt="Logo gouvernement" />
+              </p>
             </div>
           </div>
         </main>
