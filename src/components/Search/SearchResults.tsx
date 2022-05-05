a10bbb33fbd5211455182bad6669806218a5aa9c
Mathieu TUDISCO <mathieu@tudisco.fr>
2022-05-06 00:59:57 +0200

    ⚡ ️Replace Algolia search with hook lib.

--- src/components/Search/SearchResults.tsx
+++ src/components/Search/SearchResults.tsx
@@ -1,16 +1,20 @@
-import { connectHits } from 'react-instantsearch-dom'
-import type { HitsProvided } from 'react-instantsearch-core'
 import { Fragment, ReactElement } from 'react'
+import { useHits } from 'react-instantsearch-hooks'
+import { BaseHit } from 'instantsearch.js'
 import { ClassNameProp } from '../../types/react'
 
-type Props<Hit> = HitsProvided<Hit> & ClassNameProp & {renderHit: (hit: Hit) => ReactElement}
+type Props<Hit extends BaseHit> = ClassNameProp & { renderHit: (hit: Hit) => ReactElement }
 
-export const SearchResults = connectHits(({ hits, className, renderHit }: Props<any>) => (
-  <ul className={className}>
-    {hits.map(hit => (
-      <Fragment key={hit.objectID}>
-        {renderHit(hit)}
-      </Fragment>
-    ))}
-  </ul>
-))
+export const SearchResults = <Hit extends BaseHit>({ className, renderHit }: Props<Hit>) => {
+  const { hits } = useHits<Hit>()
+
+  return (
+    <ul className={className}>
+      {hits.map(hit => (
+        <Fragment key={hit.objectID}>
+          {renderHit(hit)}
+        </Fragment>
+      ))}
+    </ul>
+  )
+}
--- src/pages/fiches/[categorie]/index.tsx
+++ src/pages/fiches/[categorie]/index.tsx
@@ -1,27 +1,31 @@
 import { GetServerSideProps } from 'next'
 import { SearchIcon } from '@heroicons/react/solid'
-import { Configure } from 'react-instantsearch-dom'
 import { NextSeo } from 'next-seo'
+import { Configure } from 'react-instantsearch-hooks'
 import { Layout } from '../../../components/Layout/Layout'
 import { FicheCard } from '../../../components/Card/FicheCard'
 import { SimpleHeader } from '../../../components/Layout/SimpleHeader'
 import { SearchInput } from '../../../components/Search/SearchInput'
 import { SearchResults } from '../../../components/Search/SearchResults'
 import { SearchContext } from '../../../components/Search/SearchContext'
-import { AlgoliaSSRProps, findResultsStateForSSR } from '../../../services/algolia.browser'
+import { algoliaSSRProps, AlgoliaSSRProps, indicesNames } from '../../../services/algolia.browser'
 import { categories, CategorieSlug } from '../../../services/categories'
 import { BackToHomeLink } from '../../../components/CategorieLink'
 import { Container } from '../../../components/Layout/Container'
+import { Fiche } from '../../../types/models'
 
 type Props = AlgoliaSSRProps & { categorieSlug: CategorieSlug }
 
-export default function ListFichesByCategory({ categorieSlug, ...algoliaProps }: Props) {
+export default function ListFichesByCategory({
+  categorieSlug,
+  ...algoliaProps
+}: Props) {
   const categorie = categories[categorieSlug]
 
   return (
     <Layout className="bg-gray-50">
       <NextSeo title={categorie.name} />
-      <SearchContext {...algoliaProps}>
+      <SearchContext indexName={indicesNames.fiches} {...algoliaProps}>
         <Configure filters={`categorie:${categorieSlug}`} />
         <SimpleHeader className="h-[475px]" subTitle="Fiches pratiques" title={categorie.name} titleClassName={categorie.textColor}>
           <p className="text-sm text-gray-400 text-thin mt-4 w-3/4 mx-auto">{categorie.description}</p>
@@ -41,7 +45,7 @@ export default function ListFichesByCategory({ categorieSlug, ...algoliaProps }:
           </div>
         </SimpleHeader>
         <Container className="-mt-24 pb-12">
-          <SearchResults
+          <SearchResults<Fiche>
             className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3"
             renderHit={(hit) => <FicheCard fiche={hit} />}
           />
@@ -51,7 +55,11 @@ export default function ListFichesByCategory({ categorieSlug, ...algoliaProps }:
   )
 }
 
-export const getServerSideProps: GetServerSideProps<Props, { categorie: CategorieSlug }> = async ({ params, preview }) => {
+export const getServerSideProps: GetServerSideProps<Props, { categorie: CategorieSlug }> = async ({
+  params,
+  preview,
+  req,
+}) => {
   const categorieSlug = params!.categorie
 
   if (!categories[categorieSlug]) {
@@ -64,9 +72,7 @@ export const getServerSideProps: GetServerSideProps<Props, { categorie: Categori
     props: {
       preview: Boolean(preview || process.env.FORCE_CONTENTFUL_PREVIEW),
       categorieSlug,
-      resultsState: await findResultsStateForSSR(
-        (props) => <ListFichesByCategory categorieSlug={categorieSlug} {...props} />,
-      ),
+      ...await algoliaSSRProps(req, ListFichesByCategory, { categorieSlug }),
     },
   })
 }
