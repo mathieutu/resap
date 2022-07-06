9a053a0b527dd8442e1babaa6bff91ffe105d95b
Mathieu TUDISCO <mathieu@tudisco.fr>
2022-07-06 20:09:17 +0200

    ✨ Add progress bar when loading page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,46 @@
+import 'nprogress/nprogress.css'
 import '../assets/styles.css'
+
 import { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import { useEffect } from 'react'
+import router from 'next/router'
+import NProgress from 'nprogress'
 import { PreviewAlert } from '../components/PreviewAlert'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <DefaultSeo
-      additionalLinkTags={[{ rel: 'icon', href: '/logo.svg' }]}
-      titleTemplate="%s | Ressources Santé et Précarité"
-      defaultTitle="Ressources Santé et Précarité"
-      description="Dispositifs sociaux et médico-sociaux pour les professionnels de santé dans le but d'améliorer l'accès aux soins pour tous."
-      canonical="https://www.resap.fr"
-      openGraph={{
-        type: 'website',
-        locale: 'fr_FR',
-        site_name: 'Ressources Santé et Précarité',
-        images: [{
-          url: 'https://www.resap.fr/logo.svg',
-          alt: 'Logo Ressources Santé et Précarité',
-          type: 'image/svg',
-        }],
-      }}
-    />
-    {pageProps.preview && <PreviewAlert />}
-    <Component {...pageProps} />
-  </>
-)
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start())
+    router.events.on('routeChangeComplete', () => NProgress.done())
+    router.events.on('routeChangeError', () => NProgress.done())
+  }, [])
+
+  return (
+    <>
+      <DefaultSeo
+        additionalLinkTags={[{
+          rel: 'icon',
+          href: '/logo.svg',
+        }]}
+        titleTemplate="%s | Ressources Santé et Précarité"
+        defaultTitle="Ressources Santé et Précarité"
+        description="Dispositifs sociaux et médico-sociaux pour les professionnels de santé dans le but d'améliorer l'accès aux soins pour tous."
+        canonical="https://www.resap.fr"
+        openGraph={{
+          type: 'website',
+          locale: 'fr_FR',
+          site_name: 'Ressources Santé et Précarité',
+          images: [{
+            url: 'https://www.resap.fr/logo.svg',
+            alt: 'Logo Ressources Santé et Précarité',
+            type: 'image/svg',
+          }],
+        }}
+      />
+      {pageProps.preview && <PreviewAlert />}
+      <Component {...pageProps} />
+    </>
+  )
+}
 
 export default MyApp
