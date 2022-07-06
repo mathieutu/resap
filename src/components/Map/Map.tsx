5c4ecf4d553fbd7dca4b9b649c09605860d95d1c
Mathieu TUDISCO <mathieu@tudisco.fr>
2022-07-06 19:50:24 +0200

    🐛 Fix map Layer needing a subscription.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -66,8 +66,11 @@ export const Map = ({
         }}
       />
       <TileLayer
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+        attribution={'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'}
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
+        maxZoom={20}
+
       />
       {Object.entries(structuresByDepartements)
         .map(([departement, structs]) => (
