b4ef80db5404e15fe45c9f3ff2c064d61fc1b6c9
Mathieu TUDISCO <mathieu@tudisco.fr>
2024-05-09 22:36:58 +0200

    🧐 Export all structures entries.

--- structures2contentful.js
+++ structures2contentful.js
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,no-console,no-await-in-loop,import/no-unresolved */
-const { path } = require('ramda');
-const { writeFileSync } = require('fs');
+const { path,
+  map,
+  filter,
+  sortBy,
+} = require('ramda');
+const { writeFile } = require('node:fs/promises');
 const getContentfulEnv = require('./getContentfulEnvironment');
 const data = require('./data.json');
 
@@ -80,4 +84,26 @@ const deleteAllEntries = async () => {
   }
 };
 
-publishEntries().catch(e => console.error(e, e.action.error.details.errors.map(path(['error', 'details', 'errors']))));
+// Export all entries to JSON
+allEntries()
+  .then(filter(({ sys }) => !sys.archivedAt && sys.publishedAt))
+  .then(map(({ sys, fields }) => ({
+    id: Number(sys.id),
+    updatedAt: sys.updatedAt,
+    title: fields.nom?.fr,
+    organization: fields.organisation?.fr,
+    type: fields.type?.fr,
+    lat: fields.latLon?.fr.lat,
+    lon: fields.latLon?.fr.lon,
+    address: fields.adresse?.fr,
+    url: fields.siteWeb?.fr,
+    phone: fields.tel?.fr,
+    email: fields.email?.fr,
+  })))
+  .then(sortBy(path(['id'])))
+  .then(arr => writeFile('./export-structures.json', JSON.stringify(arr, null, 2)).then(() => arr))
+  // https://jsononline.net/fr/json-to-csv pour le CSV (tout en browser, pas d'appel API)
+  .then(console.log)
+  .catch(console.error);
+
+// publishEntries().catch(e => console.error(e, e.action.error.details.errors.map(path(['error', 'details', 'errors']))));
