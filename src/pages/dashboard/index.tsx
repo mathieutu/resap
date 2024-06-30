94e4e389ae1ebea1dfa24b6e1c6cd27ed0f246a0
Benjamin <benjamindeltour22@gmail.com>
2024-07-01 01:07:07 +0200

    🚧 Rename card into DashboardCard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,21 +1,20 @@
 import { GetStaticProps } from 'next'
 import { Layout } from '../../components/Layout/Layout'
 import { SimpleHeader } from '../../components/Layout/SimpleHeader'
-import { AlgoliaSSRProps, IndicesNames } from '../../services/algolia.browser'
 import { Container } from '../../components/Layout/Container'
 import { isPreviewForced } from '../../services/contentful'
-import { Card } from '../../components/Dashboard/Card'
+import { DashboardCard } from '../../components/Dashboard/DashboardCard'
 
-export default function Dashboard(algoliaProps: AlgoliaSSRProps) {
+export default function Dashboard() {
     return (
         <Layout className="bg-gray-light">
             <SimpleHeader className="h-[350px]" title="Dashboard" titleClassName="text-blue-default" subTitle="">
                 <div></div>
             </SimpleHeader>
             <Container className="flex justify-around -mt-24 pb-12">
-                <Card title="Fiches pratiques" link="/dashboard/fiches"/>
-                <Card title="Annuaire" link="/dashboard/structures"/>
-                <Card title="A propos" link="/dashboard/a-propos"/>
+                <DashboardCard title="Fiches pratiques" link="/dashboard/fiches"/>
+                <DashboardCard title="Annuaire" link="/dashboard/structures"/>
+                <DashboardCard title="A propos" link="/dashboard/a-propos"/>
             </Container>
         </Layout>
     )
